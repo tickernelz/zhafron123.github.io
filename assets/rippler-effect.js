126a2dc193a715281c7ac1bbd1099fed1abb7746
zhafron123 <zhafronadani@gmail.com>
2017-03-06 23:25:48 -0800

    import web

---
+++ assets/rippler-effect.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".rippler").rippler({
+    effectClass      :  'rippler-effect'
+    ,effectSize      :  16      // Default size (width & height)
+    ,addElement      :  'div'   // e.g. 'svg'(feature)
+    ,duration        :  400
+  });
+});
